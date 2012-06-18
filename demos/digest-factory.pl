80229fdabd0a8259ac5bd8546a5cca777c576357
wstlabs <nobody@devnull.org>
2012-06-18 19:53:11 +0000

    simple demo

---
+++ demos/digest-factory.pl
@@ -0,0 +1,24 @@
+#
+# a simple demonstration of anonymous factory intitialization. 
+#
+use strict;
+use warnings;
+use Class::Factory::Dict;
+use Carp;
+use YAML;
+
+our $DF = Class::Factory::Dict->new({
+    'md5'   => 'Digest::MD5',
+    'sha1'  => 'Digest::SHA1'
+});
+
+print "fac = " . Dump $DF; 
+my @keys = $DF->get_keys();
+print "keys = " . Dump \@keys; 
+
+print "instances:\n";
+for my $alg ($DF->get_keys)  {
+    my $digest = $DF->instantiate($alg);
+    print "$alg: $digest\n";
+}
+
